82ccd852c9b86401a7157b4f36d22fbbc9ba55c9
Cyprian Laskowski <cyplas@gmail.com>
2014-08-14 22:35:29 +0200

    Adapted MainActivity to show default Google Map

--- src/main/java/com/routeevents/MainActivity.java
+++ src/main/java/com/routeevents/MainActivity.java
@@ -2,14 +2,13 @@ package com.routeevents;
  
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
- 
+
 public class MainActivity extends Activity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView text = new TextView(this);
-        text.setText("Zdravo! Route Events.");
-        setContentView(text);
+        setContentView(R.layout.main_activity);
     }
+
 }
