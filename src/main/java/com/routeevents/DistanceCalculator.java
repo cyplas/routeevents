a9450f77d3872b9561970ae9afa0d615ccdffa48
Cyprian Laskowski <cyplas@gmail.com>
2014-08-16 01:50:08 +0200

    Added DistanceCalculator class for distance between route segment and event

---
+++ src/main/java/com/routeevents/DistanceCalculator.java
@@ -0,0 +1,55 @@
+package com.routeevents;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by cyp on 16.8.2014.
+ */
+public class DistanceCalculator {
+
+    public double calculateDistanceFromPointToSegment(LatLng v, LatLng w, LatLng p) {
+        double l = calculateLengthSquared(v, w);
+        if (l == 0) {
+            return square(calculateDistance(v, p));
+        } else {
+            double t = calculateParameter(v,w,p,l);
+            if (t < 0) {
+                return calculateDistance(p, v);
+            } else if (t > 1) {
+                return calculateDistance(p, w);
+            } else {
+                LatLng s = calculateProjection(v,w,t);
+                return calculateDistance(p,s);
+            }
+        }
+    }
+
+    private double square(double x) {
+        return Math.pow(x,2.0);
+    }
+
+    private double calculateDistance(LatLng a, LatLng b) {
+        double x = b.longitude - a.longitude;
+        double y = b.latitude - a.latitude;
+        return Math.sqrt(square(x) + square(y));
+    }
+
+    private double calculateLengthSquared(LatLng a, LatLng b) {
+        double x = b.longitude - a.longitude;
+        double y = b.latitude - a.longitude;
+        return square(x) + square(y);
+    }
+
+    private double calculateParameter(LatLng v, LatLng w, LatLng p, double l) {
+        double x = (p.longitude-v.longitude)*(w.longitude-v.longitude);
+        double y = (p.latitude-v.latitude)*(w.latitude-v.latitude);
+        return (x + y) / l;
+    }
+
+    private LatLng calculateProjection(LatLng v, LatLng w, double t) {
+        double x = v.longitude + t * (w.longitude-v.longitude);
+        double y = v.latitude + t * (w.latitude-v.latitude);
+        return new LatLng(y,x);
+    }
+
+}
